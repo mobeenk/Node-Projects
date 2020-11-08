c92e885b1faef85cb9324eefa8479c2e42179a51
Moubien Kayali <kayali@alamiah.lan>
2020-11-09 00:37:25 +0300

    node with NPM packages chalk,validator and the use of nodemon

---
+++ notes.js
@@ -0,0 +1,14 @@
+
+const getNotes = function(){
+    return 'notes';
+} 
+
+ module.exports = getNotes
+
+
+
+//  const getNotes = function () {
+//     return 'Your notes...'
+// }
+
+// module.exports = getNotes
